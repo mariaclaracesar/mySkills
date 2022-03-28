e6f68fc35f6d0d727e6e4dc907cf8b3d69642a4b
Maria Clara <mariaclaracesar1_5@hotmail.com>
2022-03-28 19:44:54 -0300

    adicionando typescript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,13 +11,30 @@ import {
 import { Button } from '../components/Button'
 import { SkillCard } from '../components/SkillCard'
 
+interface SkillData {
+  id: string;
+  name: string;
+  date?: Date; // com interrogação o typescript entende que vai ser opcional esse atributo
+}
+
 export function Home() {
   const [newSkill, setNewSkill] = useState('')
-  const [mySkills, setMySkills] = useState([])
+  const [mySkills, setMySkills] = useState<SkillData[]>([])
   const [greeting, setGreeting] = useState('')
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill])
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill
+    }
+
+    setMySkills(oldState => [...oldState, data])
+  }
+
+  function handleRemoveSkill(id: string){
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ))
   }
 
   useEffect(() => {
@@ -44,15 +61,23 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill} />
+      <Button 
+          title="Add"
+          onPress={handleAddNewSkill} 
+      />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
 
       <FlatList
         showsVerticalScrollIndicator={false}
         data={mySkills}
-        keyExtractor={item => item}
-        renderItem={({ item }) => <SkillCard skill={item} />}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+            <SkillCard 
+              skill={item.name} 
+              onPress={() => handleRemoveSkill(item.id)}
+            />
+        )}
       />
     </View>
   )
@@ -62,7 +87,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#121015',
-    paddingHorizontal: 20,
     paddingVertical: 70,
     paddingHorizontal: 30
   },
